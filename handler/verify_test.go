862d0b568720797bacbbf237721866343d995a4c
Igor Melo <igoracmelo@protonmail.com>
2023-02-05 20:08:25 -0300

    reduzindo repetições nos testes

--- handler/verify_test.go
+++ handler/verify_test.go
@@ -10,13 +10,17 @@ import (
 	"testing"
 )
 
+func setupVerify(body io.Reader) (res *http.Response) {
+	req := httptest.NewRequest("POST", "/verify", body)
+	w := httptest.NewRecorder()
+	handler.HandleVerify(w, req)
+	res = w.Result()
+	return
+}
+
 func TestInvalidRequestBody(t *testing.T) {
 	t.Run("nil body should return bad request", func(t *testing.T) {
-		req := httptest.NewRequest("POST", "/verify", nil)
-		w := httptest.NewRecorder()
-
-		handler.HandleVerify(w, req)
-		res := w.Result()
+		res := setupVerify(nil)
 		defer res.Body.Close()
 		assert.StatusCode(t, res.StatusCode, http.StatusBadRequest)
 
@@ -28,11 +32,7 @@ func TestInvalidRequestBody(t *testing.T) {
 	})
 
 	t.Run("empty password should return bad request", func(t *testing.T) {
-		req := httptest.NewRequest("POST", "/verify", strings.NewReader("{}"))
-		w := httptest.NewRecorder()
-
-		handler.HandleVerify(w, req)
-		res := w.Result()
+		res := setupVerify(strings.NewReader("{}"))
 		defer res.Body.Close()
 		assert.StatusCode(t, res.StatusCode, http.StatusBadRequest)
 
@@ -50,12 +50,7 @@ func TestInvalidRequestBody(t *testing.T) {
 // mas como o client que decide quais regras quer passar, optei por permitir
 func TestEmptyRules(t *testing.T) {
 	t.Run("empty 'rules' should be permitted", func(t *testing.T) {
-		body := strings.NewReader(`{"password": "123"}`)
-		req := httptest.NewRequest("POST", "/verify", body)
-		w := httptest.NewRecorder()
-
-		handler.HandleVerify(w, req)
-		res := w.Result()
+		res := setupVerify(strings.NewReader(`{"password": "123"}`))
 		defer res.Body.Close()
 		assert.StatusCode(t, res.StatusCode, http.StatusOK)
 
@@ -64,5 +59,25 @@ func TestEmptyRules(t *testing.T) {
 
 		msg := string(data)
 		assert.StringContains(t, msg, `{"verify":true,"noMatch":[]}`)
+	})
+}
+
+func TestMinSize(t *testing.T) {
+	t.Run("size less than minSize should not verify", func(t *testing.T) {
+		res := setupVerify(strings.NewReader(`{
+			"password": "123",
+			"rules": [{
+				"rule": "minSize",
+				"value": 4
+			}]
+		}`))
+		defer res.Body.Close()
+		assert.StatusCode(t, res.StatusCode, http.StatusOK)
+
+		data, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+
+		msg := string(data)
+		assert.StringContains(t, msg, `{"verify":false,"noMatch":["minSize"]}`)
 	})
 }
